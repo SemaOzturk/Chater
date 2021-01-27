cbefd39e611da0a56fc8626eb07086fc03ace288
SemaOzturk <semaozturk@outlook.it>
2021-01-27 22:52:28 +0300

    Created Project files

---
+++ ChatService.API/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChatService.API
+{
+    public class Class1
+    {
+    }
+}
---
+++ Chater.Core/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Chater.Core
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+    }
+}
